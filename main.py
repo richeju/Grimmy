f5714fd94817cbe28f78318d27f9a847521dceb8
richeju <91011913+richeju@users.noreply.github.com>
2025-03-24 21:57:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pyautogui
 
 # Activer le Fail-Safe de pyautogui
 pyautogui.FAILSAFE = True  # Déplace la souris en haut à gauche pour arrêter
-pyautogui.PAUSE = 0.1  # Petite pause entre les actions pour éviter de surcharger
+pyautogui.PAUSE = 0.1  # Petite pause entre les actions
 
 # Obtenir la résolution de l’écran
 screen_width, screen_height = pyautogui.size()
